7860894db259f7596495883d435bf46bb57d33ad
Guo Liang Oon <oon.guo.liang@gmail.com>
2025-02-21 17:26:49 +0800

    rotate by negative amount

--- array.scala
+++ array.scala
@@ -1,5 +1,7 @@
 def rotate(a: Array[Int], k: Int): Array[Int] = {
-    if (a.isEmpty) a else {
+    if (a.isEmpty) a 
+    else if (k < 0) rotate(a, a.length + (k % a.length))
+    else {
         val r = a.length - (k % a.length)
 
         (a.view ++ a.view)
--- array.test.scala
+++ array.test.scala
@@ -4,7 +4,14 @@ import munit.ScalaCheckSuite
 import org.scalacheck.Prop._
 
 class TestSuite extends munit.FunSuite with ScalaCheckSuite {
+  test("rotate, negative k") {
+    assertEquals(rotate(Array(1,2,3), -1).toSeq, Array(2,3,1).toSeq)
+    assertEquals(rotate(Array(1,2,3), -2).toSeq, Array(3,1,2).toSeq)
+    assertEquals(rotate(Array(1,2,3), -3).toSeq, Array(1,2,3).toSeq)
+  }
+
   test("rotate") {
+    assertEquals(rotate(Array[Int](), 1).toSeq, Array[Int]().toSeq)
     assertEquals(rotate(Array(1,2,3), 1).toSeq, Array(3,1,2).toSeq)
     assertEquals(rotate(Array(1,2,3), 2).toSeq, Array(2,3,1).toSeq)
     assertEquals(rotate(Array(1,2,3), 3).toSeq, Array(1,2,3).toSeq)
@@ -18,8 +25,9 @@ class TestSuite extends munit.FunSuite with ScalaCheckSuite {
 
   property("rotating by i, then j, is the same as rotating by i+j") {
     forAll { (a: Array[Int], i: Int, j: Int) =>
-      val k = (i + j) % a.length
-      assertEquals(rotate(rotate(a, i), j).toSeq, rotate(a, k).toSeq)
+      // check for integer overflow
+      (BigInt(i) + BigInt(j) > BigInt(Int.MinValue) && BigInt(i) + BigInt(j) < BigInt(Int.MaxValue)) ==>
+        assertEquals(rotate(rotate(a, i), j).toSeq, rotate(a, i+j).toSeq)
     }
   }
 }
