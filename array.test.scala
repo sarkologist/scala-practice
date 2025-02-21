86b17d8326d5dee783a665b4826c8098431e86e9
Guo Liang Oon <oon.guo.liang@gmail.com>
2025-02-21 16:09:00 +0800

    property tests

--- array.test.scala
+++ array.test.scala
@@ -1,9 +1,25 @@
 //> using dep org.scalameta::munit:1.1.0
+//> using dep org.scalameta::munit-scalacheck:1.1.0
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop._
 
-class TestSuite extends munit.FunSuite {
+class TestSuite extends munit.FunSuite with ScalaCheckSuite {
   test("rotate") {
     assertEquals(rotate(Array(1,2,3), 1).toSeq, Array(3,1,2).toSeq)
     assertEquals(rotate(Array(1,2,3), 2).toSeq, Array(2,3,1).toSeq)
     assertEquals(rotate(Array(1,2,3), 3).toSeq, Array(1,2,3).toSeq)
+  }
+
+  property("rotate by the length of the array returns original array") {
+    forAll { (a: Array[Int]) =>
+      assertEquals(rotate(a, a.length).toSeq, a.toSeq)
+    }
+  }
+
+  property("rotating by i, then j, is the same as rotating by i+j") {
+    forAll { (a: Array[Int], i: Int, j: Int) =>
+      val k = (i + j) % a.length
+      assertEquals(rotate(rotate(a, i), j).toSeq, rotate(a, k).toSeq)
+    }
   }
 }
