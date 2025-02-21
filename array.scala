f81426e8561cc142eb3e3e59cf357748f305109b
Guo Liang Oon <oon.guo.liang@gmail.com>
2025-02-21 17:40:58 +0800

    scala 3: remove brackets

--- array.scala
+++ array.scala
@@ -1,12 +1,12 @@
-def rotate(a: Array[Int], k: Int): Array[Int] = {
-    if (a.isEmpty) a 
-    else if (k < 0) rotate(a, a.length + (k % a.length))
-    else {
+def rotate(a: Array[Int], k: Int): Array[Int] =
+    if (a.isEmpty) then
+        a 
+    else if (k < 0) then
+        rotate(a, a.length + (k % a.length))
+    else
         val r = a.length - (k % a.length)
 
         (a.view ++ a.view)
         .drop(r)
         .take(a.length)
         .toArray
-    }
-}
--- array.test.scala
+++ array.test.scala
@@ -3,31 +3,29 @@
 import munit.ScalaCheckSuite
 import org.scalacheck.Prop._
 
-class TestSuite extends munit.FunSuite with ScalaCheckSuite {
-  test("rotate, negative k") {
-    assertEquals(rotate(Array(1,2,3), -1).toSeq, Array(2,3,1).toSeq)
+class TestSuite extends munit.FunSuite with ScalaCheckSuite:
+  test("rotate, negative k"):
     assertEquals(rotate(Array(1,2,3), -2).toSeq, Array(3,1,2).toSeq)
     assertEquals(rotate(Array(1,2,3), -3).toSeq, Array(1,2,3).toSeq)
-  }
+  
 
-  test("rotate") {
+  test("rotate"):
     assertEquals(rotate(Array[Int](), 1).toSeq, Array[Int]().toSeq)
     assertEquals(rotate(Array(1,2,3), 1).toSeq, Array(3,1,2).toSeq)
     assertEquals(rotate(Array(1,2,3), 2).toSeq, Array(2,3,1).toSeq)
     assertEquals(rotate(Array(1,2,3), 3).toSeq, Array(1,2,3).toSeq)
-  }
+  
 
-  property("rotate by the length of the array returns original array") {
-    forAll { (a: Array[Int]) =>
+  property("rotate by the length of the array returns original array"): 
+    forAll: (a: Array[Int]) =>
       assertEquals(rotate(a, a.length).toSeq, a.toSeq)
-    }
-  }
+    
+  
 
-  property("rotating by i, then j, is the same as rotating by i+j") {
-    forAll { (a: Array[Int], i: Int, j: Int) =>
+  property("rotating by i, then j, is the same as rotating by i+j"):
+    forAll: (a: Array[Int], i: Int, j: Int) =>
       // check for integer overflow
       (BigInt(i) + BigInt(j) > BigInt(Int.MinValue) && BigInt(i) + BigInt(j) < BigInt(Int.MaxValue)) ==>
         assertEquals(rotate(rotate(a, i), j).toSeq, rotate(a, i+j).toSeq)
-    }
-  }
-}
+    
+  
