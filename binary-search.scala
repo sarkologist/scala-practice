fb1f91eddcebcc9e7a2f70b3a3b06740cba34917
Guo Liang Oon <oon.guo.liang@gmail.com>
2025-02-23 15:53:40 +0800

    avoid intermediate list

--- binary-search.scala
+++ binary-search.scala
@@ -1,24 +1,27 @@
-def binarySearchLeq[A](target: A, xs: Vector[A])(implicit ord: Ordering[A]): Option[(A, Int)] = 
+def binarySearchLeqVector[A](target: A, xs: Vector[A])(implicit ord: Ordering[A]): Option[(A, Int)] = 
     if (xs.isEmpty) then None
-    else
-        def go(start: Int, end: Int): Option[(A, Int)] = 
-            import ord.mkOrderingOps
+    else Some(binarySearchLeq(target, xs(_), 0, xs.size - 1))
 
-            if (start <= end) then
-                val mid = (start + end) / 2
-                xs(mid) match {
-                    case x if x == target => Some((x, mid))
-                    case x if x < target => go(mid + 1, end)
-                    case x if x > target => go(start, mid - 1)
-                    case _ => throw new RuntimeException("unreachable: violates trichotomy")
-                }
-            else Some((xs(Math.max(0, end)), Math.max(0, end)))
+def binarySearchLeq[A](target: A, index: Int => A, left: Int, right: Int)(implicit ord: Ordering[A]): (A, Int) = 
+    def go(start: Int, end: Int): (A, Int) = 
+        import ord.mkOrderingOps
 
-        go(0, xs.length - 1)
+        if (start <= end) then
+            val mid = (start + end) / 2
+            index(mid) match {
+                case x if x == target => (x, mid)
+                case x if x < target => go(mid + 1, end)
+                case x if x > target => go(start, mid - 1)
+                case _ => throw new RuntimeException("unreachable: violates trichotomy")
+            }
+        else (index(Math.max(left, end)), Math.max(left, end))
+
+    go(left, right)
 
 def squareRoot(n: Int): Int =
     n match {
         case 0 => 0
         case 1 => 1
-        case _ => binarySearchLeq(n, (0 to n / 2).map(x => x * x).toVector).get._2
+        case _ => binarySearchLeq(n, (x) => x*x, 0, n / 2)._2
     }
+
--- binary-search.test.scala
+++ binary-search.test.scala
@@ -3,14 +3,14 @@ import org.scalacheck.Prop._
 
 class BinarySearchTestSuite extends munit.FunSuite with ScalaCheckSuite:
     test("binarySearchLeq"):
-        assertEquals(binarySearchLeq(0, Vector(0)), Some((0, 0)))
-        assertEquals(binarySearchLeq(1, Vector(0)), Some((0, 0)))
-        assertEquals(binarySearchLeq(-1, Vector(0)), Some((0, 0)))
+        assertEquals(binarySearchLeqVector(0, Vector(0)), Some((0, 0)))
+        assertEquals(binarySearchLeqVector(1, Vector(0)), Some((0, 0)))
+        assertEquals(binarySearchLeqVector(-1, Vector(0)), Some((0, 0)))
 
-        assertEquals(binarySearchLeq(0, Vector(0,2)), Some((0, 0)))
-        assertEquals(binarySearchLeq(1, Vector(0,2)), Some((0, 0)))
-        assertEquals(binarySearchLeq(2, Vector(0,2)), Some((2, 1)))
-        assertEquals(binarySearchLeq(3, Vector(0,2)), Some((2, 1)))
+        assertEquals(binarySearchLeqVector(0, Vector(0,2)), Some((0, 0)))
+        assertEquals(binarySearchLeqVector(1, Vector(0,2)), Some((0, 0)))
+        assertEquals(binarySearchLeqVector(2, Vector(0,2)), Some((2, 1)))
+        assertEquals(binarySearchLeqVector(3, Vector(0,2)), Some((2, 1)))
 
     test("squareRoot"):
         assertEquals(squareRoot(0), 0)
